6ad7b0df858c056b45f971c42ac0d10c90e10e2f
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2021-11-27 15:32:22 -0600

    removes feed

--- src/server.ts
+++ src/server.ts
@@ -5,12 +5,10 @@ import {sequelize} from './sequelize';
 import {IndexRouter} from './controllers/v0/index.router';
 
 import bodyParser from 'body-parser';
-import {config} from './config/config';
-import {V0_FEED_MODELS, V0_USER_MODELS} from './controllers/v0/model.index';
+import {V0_USER_MODELS} from './controllers/v0/model.index';
 
 
 (async () => {
-  await sequelize.addModels(V0_FEED_MODELS);
   await sequelize.addModels(V0_USER_MODELS);
 
   console.debug("Initialize database connection...");
@@ -38,9 +36,9 @@ import {V0_FEED_MODELS, V0_USER_MODELS} from './controllers/v0/model.index';
   app.use('/api/v0/', IndexRouter);
 
   // Root URI call
-  app.get( '/', async ( req, res ) => {
-    res.send( 'Nothing here.' );
-  } );
+  app.get('/', async (req, res) => {
+    res.send('Nothing here.');
+  });
 
   // Health
   app.get('/health', (req, res) => {
@@ -49,8 +47,8 @@ import {V0_FEED_MODELS, V0_USER_MODELS} from './controllers/v0/model.index';
 
 
   // Start the Server
-  app.listen( port, () => {
-    console.log( `server running on port ${port}` );
-    console.log( `press CTRL+C to stop server` );
-  } );
+  app.listen(port, () => {
+    console.log(`server running on port ${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
 })();

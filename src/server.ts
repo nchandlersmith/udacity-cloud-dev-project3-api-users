497e6eb8e14df08554f7065e6cfc6a7cb329d4e7
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2021-11-30 22:21:43 -0600

    ads cors headers manual

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,8 @@
-import cors from 'cors';
 import express from 'express';
 import {sequelize} from './sequelize';
 
 import {IndexRouter} from './controllers/v0/index.router';
 
-import bodyParser from 'body-parser';
 import {V0_USER_MODELS} from './controllers/v0/model.index';
 
 
@@ -17,12 +15,11 @@ import {V0_USER_MODELS} from './controllers/v0/model.index';
   const app = express();
   const port = process.env.PORT || 8080;
 
-  app.use(bodyParser.json());
-
-  // We set the CORS origin to * so that we don't need to
-  // worry about the complexities of CORS this lesson. It's
-  // something that will be covered in the next course.
-  app.use(cors());
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+  });
 
   app.use('/api/v0/', IndexRouter);
 

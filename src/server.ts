096a20b9782841094819af12cd868e64a8e38b37
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2021-11-30 20:38:50 -0600

    removes dead code

--- src/server.ts
+++ src/server.ts
@@ -24,12 +24,6 @@ import {V0_USER_MODELS} from './controllers/v0/model.index';
   // something that will be covered in the next course.
   app.use(cors());
 
-  app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    next();
-  });
-
   app.use('/api/v0/', IndexRouter);
 
   // Root URI call
